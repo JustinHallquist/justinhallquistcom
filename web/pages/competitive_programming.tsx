fa8eccc1a1a45a8d30bbc17fd143037d4100e10c
Justin Hallquist <justin.hallquist@signalpet.com>
2022-07-03 14:17:17 -0400

    add sidebar

--- web/pages/competitive_programming.tsx
+++ web/pages/competitive_programming.tsx
@@ -3,9 +3,24 @@ import Layout from './layouts/layout'
 export default function CompetitiveProgramming() {
   return (
     <Layout content={
-      <h1 className="h-screen text-3xl font-bold underline">
-        Competitive Programming
-      </h1>
+      <section className="h-screen bg-white dark:bg-gray-900">
+        <div className="container px-6 py-8 mx-auto">
+          <div className="lg:flex lg:-mx-2">
+            <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
+              <a href="#" className="block font-medium text-gray-500 dark:text-gray-300 hover:underline">usaco</a>
+              <a href="#" className="block font-medium text-gray-500 dark:text-gray-300 hover:underline">codeforces</a>
+            </div>
+
+            <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
+              <div className="flex items-center justify-between text-sm tracking-widest uppercase ">
+              </div>
+
+              <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     } />
   )
 }
