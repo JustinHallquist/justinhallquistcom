76e8bb3680bdf3a0cf80dad1c31c99524ac1caad
Justin Hallquist <justin.hallquist@signalpet.com>
2022-07-04 13:37:54 -0400

    closer

--- web/pages/competitive_programming.tsx
+++ web/pages/competitive_programming.tsx
@@ -5,20 +5,16 @@ import SectionTabs from './components/section_tabs';
 import competitiveProgrammingResources from '../competitive_programming/resources.json'
 
 const sites = competitiveProgrammingResources.problems
-const defaultSite = Object.keys(sites)[0]
-const defaultProblem = Object.keys(sites[defaultSite])[0]
 
 const sections = ['problem', 'solution', 'explanation']
 const defaultSection = 'problem'
 
 export default function CompetitiveProgramming() {
-  const [activeSite, setSite] = useState(defaultSite);
-  const [activeProblem, setProblem] = useState(defaultProblem);
+  const [activePath, setActivePath] = useState('');
   const [activeSection, setSection] = useState(defaultSection);
 
   // TODO: type
-  const handleSetSite = (site: String) => setSite(site)
-  const handleSetProblem = (problem: String) => setProblem(problem)
+  const handleSetActivePath = (path: String) => setActivePath(path)
   const handleSetActiveSection = (section: String) => setSection(section)
 
   return (
@@ -26,7 +22,7 @@ export default function CompetitiveProgramming() {
       <section className="h-full bg-white dark:bg-gray-900">
         <div className="container px-6 py-8 mx-auto">
           <div className="lg:flex lg:-mx-2">
-            <TreeNav tree={sites} activeNode={activeSite} />
+            <TreeNav tree={sites} activeNode={activePath} handleSetActivePath={handleSetActivePath} ignore={['files']} />
             <SectionTabs sections={sections} activeSection={activeSection} handleSetActiveSection={handleSetActiveSection} />
 
             <div className="grid grid-cols-1 gap-4 mt-4 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
--- web/pages/components/tree_nav.tsx
+++ web/pages/components/tree_nav.tsx
@@ -1,10 +1,10 @@
-export default function TreeNav({ tree, handleSetActivePath }) {
+export default function TreeNav({ tree, activeNode, ignore, handleSetActivePath }) {
   const dom = []
 
-  const rootNodeEl = (node, active, path, depth) => <div path={path} onClick={() => handleSetActiveNode(node)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'}`}  >{node}</div>
-  const nodeEl = (node, active, path, depth) => {
+  const rootNodeEl = (node, active, path, depth) => <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'}`}  >{node}</div>
+  const nodeEl = (node, active, parentActive, path, depth) => {
     const tabs = Array(depth * 2).fill(0).reduce((memo, _) => `${memo}-`, '');
-    return <div path={path} onClick={() => handleSetActiveNode(node)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'}`}  ><span>{tabs} </span>{node}</div>
+    return <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'} ${!parentActive ? 'hidden' : ''}`}  ><span>{tabs} </span>{node}</div>
   }
 
   const build = (nodes, dom, activeNode = '', path = '', depth = 0) => {
@@ -13,31 +13,31 @@ export default function TreeNav({ tree, handleSetActivePath }) {
         const curPath = path.length ? `${path}.${node}` : node
         const active = activeNode.startsWith(curPath)
 
-        if (node === 'files') return // TODO pass path ignore
+        const pathParts = curPath.split('.').filter(Boolean)
+        const parentActive = pathParts.length && activeNode.startsWith(pathParts.slice(0, pathParts.length - 1).join('.'))
+
+        if (ignore.includes(node)) return // TODO pass path ignore
 
         if (nodes[node]) {
           if (depth === 0) {
             dom.push([
               rootNodeEl(node, active, curPath, depth)
-              // 
             ])
           } else {
             dom.push([
-              nodeEl(node, active, curPath, depth)
+              nodeEl(node, active, parentActive, curPath, depth)
             ])
           }
 
-          build(nodes[node], dom, activeNode, curPath, depth + 1)
-        } else {
           dom.push([
-            nodeEl(node, active, activeNode === path, depth)
+            build(nodes[node], dom, activeNode, curPath, depth + 1)
           ])
         }
       })
     }
   }
 
-  build(tree, dom, 'usaco.section_1.barn1')
+  build(tree, dom, activeNode)
 
   return <>
     <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
