f84a6a07911b23072302dc94ce751fc2ee48dd40
Justin Hallquist <justin.hallquist@signalpet.com>
2022-07-04 13:40:06 -0400

    k

--- web/pages/components/tree_nav.tsx
+++ web/pages/components/tree_nav.tsx
@@ -1,10 +1,10 @@
 export default function TreeNav({ tree, activeNode, ignore, handleSetActivePath }) {
   const dom = []
 
-  const rootNodeEl = (node, active, path, depth) => <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'}`}  >{node}</div>
+  const rootNodeEl = (node, active, path, depth) => <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`cursor-pointer block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'}`}  >{node}</div>
   const nodeEl = (node, active, parentActive, path, depth) => {
     const tabs = Array(depth * 2).fill(0).reduce((memo, _) => `${memo}-`, '');
-    return <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'} ${!parentActive ? 'hidden' : ''}`}  ><span>{tabs} </span>{node}</div>
+    return <div path={path} onClick={() => handleSetActivePath(path)} key={`${node}.${depth}`} className={`cursor-pointer block font-medium hover:underline ${active ? 'dark:text-blue-500' : 'dark:text-gray-300'} ${!parentActive ? 'hidden' : ''}`}  ><span>{tabs} </span>{node}</div>
   }
 
   const build = (nodes, dom, activeNode = '', path = '', depth = 0) => {
@@ -39,9 +39,7 @@ export default function TreeNav({ tree, activeNode, ignore, handleSetActivePath 
 
   build(tree, dom, activeNode)
 
-  return <>
-    <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
-      {dom}
-    </div >
-  </>
+  return <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
+    {dom}
+  </div >
 }
